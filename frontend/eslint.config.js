57ae4293523723934cdc9670ce4b7a4c8113db1b
Mounir BELHNIYENE <mounir.blhn@gmail.com>
2025-05-06 16:08:19 +0100

    chore(frontend): add prettier and integrate it with eslint and npm scripts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,37 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tsEslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import {defineConfig} from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tsEslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
-  {ignores: ['dist']},
+  { ignores: ['dist'] },
   js.configs.recommended,
   ...tsEslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
-        }
-      }
+          jsx: true,
+        },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [prettier],
     rules: {
+      'react/react-in-jsx-scope': 'off',
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        {allowConstantExport: true},
-      ],
-    }
-  }
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
 ]);
